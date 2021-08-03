bc76f5033a6790f0deebdce251c5681726f844a3
anurag peche <anuragpeche1999@gmail.com>
2021-08-04 01:00:30 +0530

    okk

--- p2c/urls.py
+++ p2c/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
          
